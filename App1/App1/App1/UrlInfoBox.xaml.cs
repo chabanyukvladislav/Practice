bec17a533c72235b09427b61502d678199b69873
Влад Чабанюк <vlad19112000@gmail.com>
2018-08-29 17:51:09 +0300

    up

--- App1/App1/App1/UrlInfoBox.xaml.cs
+++ App1/App1/App1/UrlInfoBox.xaml.cs
@@ -6,16 +6,30 @@ namespace App1
     public partial class UrlInfoBox
     {
         public static readonly BindableProperty ElementProperty;
+        public static readonly BindableProperty LeftMarginProperty;
+        public static readonly BindableProperty TopMarginProperty;
 
         public UrlInfo Element
         {
             get => GetValue(ElementProperty) as UrlInfo;
             set => SetValue(ElementProperty, value);
         }
+        public double LeftMargin
+        {
+            get => (double)GetValue(LeftMarginProperty);
+            set => SetValue(LeftMarginProperty, value);
+        }
+        public double TopMargin
+        {
+            get => (double)GetValue(TopMarginProperty);
+            set => SetValue(TopMarginProperty, value);
+        }
 
         static UrlInfoBox()
         {
             ElementProperty = BindableProperty.Create(nameof(Element), typeof(UrlInfo), typeof(UrlInfoBox), new UrlInfo() { Arguments = new Dictionary<string, string>() });
+            LeftMarginProperty = BindableProperty.Create(nameof(LeftMargin), typeof(double), typeof(UrlInfoBox),0.0);
+            TopMarginProperty = BindableProperty.Create(nameof(TopMargin), typeof(double), typeof(UrlInfoBox), 0.0);
         }
 
         public UrlInfoBox()
@@ -25,8 +39,22 @@ namespace App1
 
         protected override void OnPropertyChanged(string propertyName = null)
         {
+            base.OnPropertyChanged(propertyName);
+
             if (propertyName == ElementProperty.PropertyName)
                 BindingContext = Element;
+            else if (propertyName == LeftMarginProperty.PropertyName)
+            {
+                Thickness margin = UrlStackPanel.Margin;
+                margin.Left = LeftMargin;
+                UrlStackPanel.Margin = margin;
+            }
+            else if (propertyName == TopMarginProperty.PropertyName)
+            {
+                Thickness margin = UrlStackPanel.Margin;
+                margin.Top = TopMargin;
+                UrlStackPanel.Margin = margin;
+            }
         }
     }
 }
--- App1/App1/App1/UrlTextBox.xaml.cs
+++ App1/App1/App1/UrlTextBox.xaml.cs
@@ -5,16 +5,30 @@ namespace App1
     public partial class UrlTextBox
     {
         public static readonly BindableProperty UrlTextProperty;
+        public static readonly BindableProperty LeftMarginProperty;
+        public static readonly BindableProperty TopMarginProperty;
 
         public string UrlText
         {
             get => GetValue(UrlTextProperty).ToString();
             set => SetValue(UrlTextProperty, value);
         }
+        public double LeftMargin
+        {
+            get => (double)GetValue(LeftMarginProperty);
+            set => SetValue(LeftMarginProperty, value);
+        }
+        public double TopMargin
+        {
+            get => (double)GetValue(TopMarginProperty);
+            set => SetValue(TopMarginProperty, value);
+        }
 
         static UrlTextBox()
         {
             UrlTextProperty = BindableProperty.Create(nameof(UrlText), typeof(string), typeof(UrlTextBox), "http://google.com.ua?id=123&text=test", BindingMode.TwoWay);
+            LeftMarginProperty = BindableProperty.Create(nameof(LeftMargin), typeof(double), typeof(UrlTextBox), 0.0);
+            TopMarginProperty = BindableProperty.Create(nameof(TopMargin), typeof(double), typeof(UrlTextBox), 0.0);
         }
 
         public UrlTextBox()
@@ -36,6 +50,18 @@ namespace App1
 
             if (propertyName == UrlTextProperty.PropertyName && UrlText != UrlEntry.Text)
                 UrlEntry.Text = UrlText;
+            else if (propertyName == LeftMarginProperty.PropertyName)
+            {
+                Thickness margin = UrlEntry.Margin;
+                margin.Left = LeftMargin;
+                UrlEntry.Margin = margin;
+            }
+            else if (propertyName == TopMarginProperty.PropertyName)
+            {
+                Thickness margin = UrlEntry.Margin;
+                margin.Top = TopMargin;
+                UrlEntry.Margin = margin;
+            }
         }
     }
 }
